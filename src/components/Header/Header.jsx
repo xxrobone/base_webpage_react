bbaea880bd40049b0c24283a5fece13c6d2c96a4
robonexx <robertwagar@gmail.com>
2023-02-04 12:02:34 +0100

    react base structure for website early stage

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import office from '../../videos/office.mp4'
+
+// styles
+import './Header.scss'
+
+const Header = (props) => {
+ 
+    return (
+        <React.Fragment>
+      
+          <header id="home">
+             
+          <video autoPlay loop muted
+                    style={{
+                       position: "absolute",
+                       top: '0',
+                       left: '0',
+                        width: "100%",
+                        height: "80%",
+                        zIndex: "-1",
+                        objectFit: "cover",
+
+                    }}
+                >
+                    <source src={office} type="video/mp4" />
+                </video>
+         
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#about">About</a></li>
+             <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
+               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul>
+         </nav>
+
+         <div className="row banner">
+            <div className="banner-text">
+               <h1 className="responsive-headline">I am {props.pageData.name}.</h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{props.pageData.description}
+               </h3>
+               <hr/>
+               <ul className="social">
+                  {
+                    props.pageData.socialLinks && props.pageData.socialLinks.map(item =>{
+                      return(
+                              <li key={item.name}>
+                                <a href={item.url} target="_blank" rel='noopener noreferrer'><i className={item.className}></i></a>
+                              </li>
+                            )
+                          }
+                    )
+                  }
+               </ul>
+            </div>
+         </div>
+
+         <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+         </p>
+
+      </header>
+      </React.Fragment>
+    );
+}
+export default Header;
+
---
+++ src/data/pageData.js
@@ -0,0 +1,156 @@
+export const pageData = {
+  name: 'Robert Wägar',
+  description:
+    "I´m a developer with focus on creating smart user interfaces & useful interactions \nMy design is equally about what it does as much as how it attracts the eye \nIf you are looking for a developer to bring your ideas to life, You have come to the right place \n As a developer I'm focused on finding the best solutions for you problems/ needs by developing rich web experiences & applications for your users \n when not coding I work with my other passion dance focus on locking & hip-hop",
+  socials: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/robert-w%C3%A4gar-1b4661139/',
+      className: 'fa-brands fa-linkedin-in',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/robonexx',
+      className: 'fa-brands fa-github',
+    },
+    {
+      name: 'twitter',
+      url: 'https://codepen.io/robonexx',
+      className: 'fa-brands fa-codepen',
+    },
+  ],
+  about:
+    "I'm a Finn born and raised in Sweden, dad of 2 boys, Frontend developer on the road to Full Stack, with 3 years of experience. My education and work have provided me with a variety of knowledge on technologies used in development, theoretical and practical skills. I use this knowledge in my projects, and I still look forward to learn more. I consider myself to be a openminded, problem solving, and capable worker, looking on things from a brighter side of life, not easily affected by stress. I build easy to use, user friendly interactions, websites and apps,  truly a passion of mine. Learning new as I go forwards and staying up-to-date on the tech and what's new.",
+  website: 'https://www.robertwagar.se',
+  education: [
+    {
+      education: 'KYH',
+      specialization: 'Frontend developer',
+      MonthOfPassing: 'Jun',
+      YearOfPassing: '2022',
+      Achievements: 'Top grade in 95% of the subjects',
+    },
+    {
+      education: 'CME',
+      specialization: 'App / Frontend',
+      MonthOfPassing: 'ongoing',
+      YearOfPassing: 'ongoing',
+      Achievements: 'Top grade 100% so far',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'LockerLegends',
+      specialization: 'Web developer',
+      MonthOfLeaving: 'Ongoing',
+      YearOfLeaving: '2022',
+    },
+    {
+      CompanyName: 'Addcode',
+      specialization: 'Web development using wordpress and align',
+      MonthOfLeaving: 'April',
+      YearOfLeaving: '2022',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS/SASS',
+    },
+    {
+      skillname: 'Javascript',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+    {
+      skillname: 'Nextjs',
+    },
+    {
+      skillname: 'Nodejs',
+    },
+    {
+      skillname: 'Rest-Api',
+    },
+    {
+      skillname: 'Wordpress',
+    },
+    {
+      skillname: 'Framer-motion',
+    },
+    {
+      skillname: 'Anime.js',
+    },
+    {
+      skillname: 'GraphQl',
+    },
+    {
+      skillname: 'MongoDb',
+    },
+    {
+      skillname: 'Firebase',
+    },
+    {
+      skillname: 'MySql',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'LockerLegends',
+      description: 'webapp',
+      imgurl: 'images/modal/coffee.jpg',
+    },
+    {
+      name: 'VGA',
+      description: 'mobileapp',
+      imgurl: 'images/modal/console.jpg',
+    },
+    {
+      name: 'Qoutes by Roger',
+      description: 'mobileapp',
+      imgurl: 'images/modal/farmerboy.jpg',
+    },
+    {
+      name: 'SL app',
+      description: 'mobileapp',
+      imgurl: 'images/modal/retrocam.jpg',
+    },
+    {
+      name: 'Coming soon',
+      description: 'mobileapp',
+      imgurl: 'images/modal/girl.jpg',
+    },
+    {
+      name: 'Coming soon',
+      description: 'mobileapp',
+      imgurl: 'images/modal/into-the-light.jpg',
+    },
+    {
+      name: 'Coming soon',
+      description: 'mobileapp',
+      imgurl: 'images/modal/judah.jpg',
+    },
+    {
+      name: 'Coming soon',
+      description: 'mobileapp',
+      imgurl: 'images/modal/origami.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Skeeter Higgins',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Anthony Lewis',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Anette Starck',
+    },
+  ],
+};

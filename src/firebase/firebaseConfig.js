d3a9b3477552174dacec831177c83671cd9a5c38
robonexx <robertwagar@gmail.com>
2023-02-08 07:09:20 +0100

    oopsy, added env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,18 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 //import from firestore
 const firebaseConfig = {
-  /* apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID, */
-  apiKey: 'AIzaSyAQ-yMfz07MweLlGguSjJum96jhlOWpuiw',
-  authDomain: 'rjwportfolio-6e348.firebaseapp.com',
-  projectId: 'rjwportfolio-6e348',
-  storageBucket: 'rjwportfolio-6e348.appspot.com',
-  messagingSenderId: '113294750820',
-  appId: '1:113294750820:web:3d6105123e60c067df9b71',
+  /* apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID, */
+  
 };
 
 // init firebase

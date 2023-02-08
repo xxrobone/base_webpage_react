adc3f51b9c3aa90f214326432b25185aaa0b8622
robonexx <robertwagar@gmail.com>
2023-02-08 07:38:53 +0100

    nav animations with framer motion

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {motion as mt } from 'framer-motion'
 
 // styles
 import './NavItem.scss'
 
-export default function NavItem({title, path, cls, id}) {
+export default function NavItem({title, path, cls, id, i}) {
   return (
-      <li className={cls} key={id}>
+    <mt.li className={cls} key={id}
+    initial={{ opacity: 0, y: i % 2 === 0 ? -100 : 100 }}
+              animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: i * 0.2 }}
+    >
           <Link className="nav_link" to={path}>{title}</Link>
-    </li>
+    </mt.li>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {motion as mt } from 'framer-motion'
 
 const Home = () => {
   return (
       <div>
-          <h1>homepage</h1>
+      <h1>homepage</h1>
+      <div className="info">
+
+      </div>
     </div>
   )
 }
